08fc276ba8c8d8d2b3ede0df63a625349edda8ed
Chris <chrisshen93@gmail.com>
2019-07-24 11:48:56 +0800

    github 设置页面

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,14 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<SettingsPage> {
+  GlobalKey _formKey = new GlobalKey<FormState>();
+
+  void saveSettings () {
+    (_formKey.currentState as FormState).save();
+  }
+
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
       appBar: AppBar(
         title: Text('设置页')
       ),
-      body: Text('设置')
+      body: Padding(
+        // padding: EdgeInsets.all(10),
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                autovalidate: true,
+                decoration: InputDecoration(
+                  labelText: '设定仓库名',
+                  hintText: '格式：username/repo',
+                  prefixIcon: Icon(Icons.home),
+                ),
+                validator: (v) {
+                  return v.trim().length > 0 ? null : '仓库名不可为空';
+                },
+              ),
+              TextFormField(
+                autovalidate: true,
+                decoration: InputDecoration(
+                  labelText: '设定分支名',
+                  hintText: '例如：master',
+                  prefixIcon: Icon(Icons.call_split),
+                ),
+                validator: (v) {
+                  return v.trim().length > 0 ? null : '分支名不可为空';
+                },
+              ),
+              TextFormField(
+                autovalidate: true,
+                decoration: InputDecoration(
+                  labelText: '设定Token',
+                  hintText: 'token',
+                  prefixIcon: Icon(Icons.vpn_key),
+                ),
+                validator: (v) {
+                  return v.trim().length > 0 ? null : 'Token不可为空';
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '指定存储路径',
+                  hintText: '例如：img/',
+                  prefixIcon: Icon(Icons.folder),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '设定自定义域名',
+                  hintText: '例如：https://xxx.com',
+                  prefixIcon: Icon(Icons.domain),
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text('保存'),
+                        onPressed: () {
+                          saveSettings();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
