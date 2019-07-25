fabcaa793944461530fa46095feead929c7830e6
ChrisShen93 <chrisshen93@gmail.com>
2019-07-25 10:58:14 +0800

    fix: call length on null; fix: bottom overflowed by pixels on android

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -25,14 +25,19 @@ class _SettingsState extends State<SettingsPage> {
 
   void _initPrefs () async {
     prefs = await SharedPreferences.getInstance();
-    GithubSetting setting = new GithubSetting.fromJson(jsonDecode(prefs.getString('github_settings')));
-    setState(() {
-      _repoController.text = setting.repo;
-      _branchController.text = setting.branch;
-      _tokenController.text = setting.token;
-      _pathController.text = setting.path;
-      _customUrlController.text = setting.customUrl;
-    });
+    var settingFromSharedPreferences = prefs.getString('github_setting');
+    if (settingFromSharedPreferences != null && settingFromSharedPreferences.length > 0) {
+      GithubSetting setting = new GithubSetting.fromJson(jsonDecode(settingFromSharedPreferences));
+      if (setting.repo != '') {
+        setState(() {
+          _repoController.text = setting.repo;
+          _branchController.text = setting.branch;
+          _tokenController.text = setting.token;
+          _pathController.text = setting.path;
+          _customUrlController.text = setting.customUrl;
+        });
+      }
+    }
   }
 
   void saveSettings () async {
@@ -60,80 +65,82 @@ class _SettingsState extends State<SettingsPage> {
       body: Padding(
         // padding: EdgeInsets.all(10),
         padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                autofocus: true,
-                autovalidate: true,
-                controller: _repoController,
-                decoration: InputDecoration(
-                  labelText: '设定仓库名',
-                  hintText: '格式：username/repo',
-                  prefixIcon: Icon(Icons.home),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  autovalidate: true,
+                  controller: _repoController,
+                  decoration: InputDecoration(
+                    labelText: '设定仓库名',
+                    hintText: '格式：username/repo',
+                    prefixIcon: Icon(Icons.home),
+                  ),
+                  validator: (v) {
+                    return v.trim().length > 0 ? null : '仓库名不可为空';
+                  },
                 ),
-                validator: (v) {
-                  return v.trim().length > 0 ? null : '仓库名不可为空';
-                },
-              ),
-              TextFormField(
-                autovalidate: true,
-                controller: _branchController,
-                decoration: InputDecoration(
-                  labelText: '设定分支名',
-                  hintText: '例如：master',
-                  prefixIcon: Icon(Icons.call_split),
+                TextFormField(
+                  autovalidate: true,
+                  controller: _branchController,
+                  decoration: InputDecoration(
+                    labelText: '设定分支名',
+                    hintText: '例如：master',
+                    prefixIcon: Icon(Icons.call_split),
+                  ),
+                  validator: (v) {
+                    return v.trim().length > 0 ? null : '分支名不可为空';
+                  },
                 ),
-                validator: (v) {
-                  return v.trim().length > 0 ? null : '分支名不可为空';
-                },
-              ),
-              TextFormField(
-                autovalidate: true,
-                controller: _tokenController,
-                decoration: InputDecoration(
-                  labelText: '设定Token',
-                  hintText: 'token',
-                  prefixIcon: Icon(Icons.vpn_key),
+                TextFormField(
+                  autovalidate: true,
+                  controller: _tokenController,
+                  decoration: InputDecoration(
+                    labelText: '设定Token',
+                    hintText: 'token',
+                    prefixIcon: Icon(Icons.vpn_key),
+                  ),
+                  validator: (v) {
+                    return v.trim().length > 0 ? null : 'Token不可为空';
+                  },
                 ),
-                validator: (v) {
-                  return v.trim().length > 0 ? null : 'Token不可为空';
-                },
-              ),
-              TextFormField(
-                controller: _pathController,
-                decoration: InputDecoration(
-                  labelText: '指定存储路径',
-                  hintText: '例如：img/',
-                  prefixIcon: Icon(Icons.folder),
+                TextFormField(
+                  controller: _pathController,
+                  decoration: InputDecoration(
+                    labelText: '指定存储路径',
+                    hintText: '例如：img/',
+                    prefixIcon: Icon(Icons.folder),
+                  ),
                 ),
-              ),
-              TextFormField(
-                controller: _customUrlController,
-                decoration: InputDecoration(
-                  labelText: '设定自定义域名',
-                  hintText: '例如：https://xxx.com',
-                  prefixIcon: Icon(Icons.domain),
+                TextFormField(
+                  controller: _customUrlController,
+                  decoration: InputDecoration(
+                    labelText: '设定自定义域名',
+                    hintText: '例如：https://xxx.com',
+                    prefixIcon: Icon(Icons.domain),
+                  ),
                 ),
-              ),
 
-              Padding(
-                padding: EdgeInsets.only(top: 30),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RaisedButton(
-                        child: Text('保存'),
-                        onPressed: () {
-                          saveSettings();
-                        },
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          child: Text('保存'),
+                          onPressed: () {
+                            saveSettings();
+                          },
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
